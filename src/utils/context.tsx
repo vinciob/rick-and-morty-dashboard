59cab16301b853e9e5f72d119c47b26f007c6c54
vinciob <vincio5000@hotmail.it>
2022-07-02 12:50:47 +0200

    implement a cookie for favorites characters

--- src/utils/context.tsx
+++ src/utils/context.tsx
@@ -1,23 +1,52 @@
 import { createContext, useState } from "react";
+import Cookies from 'universal-cookie';
 
 // Declare Context
 const GlobalContext = createContext<any>({});
 
+
 // Define Provider wrapper and functions
 function GlobalProvider({children} : any) {
+    // Cookie
+    const cookies = new Cookies();
+    const cookieFavorites : number[] | [] = cookies.get('favorites') || []
     // Define Global States
     const [idCharacterSelected, setIdCharacterSelected] = useState<number | null>(null)
     const [querySearch, setQuerySearch] = useState<string>()
-    const [favoritesCharacters, setFavoritesCharacters] = useState<string[]>([])
+    const [favoritesCharacters, setFavoritesCharacters] = useState<number[] | []>(cookieFavorites)
+    
 
     // Add character ID in favorites array
-    const addFavoritesCharacter = (id: string) => {
-        setFavoritesCharacters( prevState => [...prevState, id] )
+    const addFavoritesCharacter = (id: number) => {
+        setFavoritesCharacters( prevState => {
+            const favoritesArray = [...prevState, id]
+            
+            // Handle Cookie
+            const now = new Date()
+            cookies.set('favorites', favoritesArray, { 
+                path: '/' ,
+                expires: new Date(now.getTime() + 1000000000),
+                sameSite: true
+            });
+            
+            return favoritesArray
+        })
     }
+                
 
     //Remove character ID in favorites array
-    const removeFavoritesCharacter = (id: string) => {
-        setFavoritesCharacters( prevState => prevState.filter(idCharacter => idCharacter !== id) )
+    const removeFavoritesCharacter = (id: number) => {
+        const favoritesArray = favoritesCharacters.filter( (idCharacter: number) => idCharacter !== id)
+        
+        // Handle Cookie
+        const now = new Date()
+        cookies.set('favorites', favoritesArray, { 
+            path: '/' ,
+            //expires: new Date(now.getTime() + 1000000000),
+            sameSite: true
+        });
+        
+        setFavoritesCharacters(favoritesArray)
     }
 
     return <GlobalContext.Provider value={{
